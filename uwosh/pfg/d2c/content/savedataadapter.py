1bf5fcfe3b582693a74d4e82ba97e0b5c0e9e5e5
vangheem <vangheem@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-07-29 21:30:17 +0000

    avoid security checks when creating content from form.

--- uwosh/pfg/d2c/content/savedataadapter.py
+++ uwosh/pfg/d2c/content/savedataadapter.py
@@ -10,26 +10,51 @@ from zope.interface import implements
 from Products.Archetypes.Field import FileField
 from Products.PloneFormGen.content.likertField import LikertField
 from Products.Archetypes.Field import ObjectField
+from Products.CMFCore.utils import getToolByName
+
+FormSaveData2ContentAdapterSchema = ATFolderSchema.copy() + FormAdapterSchema.copy() + \
+    Schema((
+        BooleanField('avoidSecurityChecks',
+            default=True,
+            widget=BooleanWidget(label="Avoid Security Checks",
+                description="""
+                Avoid checking if the user has permission to create the content
+                data. You will almost always want this checked; otherwise,
+                anonymous users will most likely not be able to submit your
+                forms.
+                """,)
+        )
+    ))
 
 class FormSaveData2ContentAdapter(ATFolder, FormActionAdapter):
     """A form action adapter that will save form input data and 
        return it in csv- or tab-delimited format."""
 
     implements(IFormSaveData2ContentAdapter)
-    schema = ATFolderSchema.copy() + FormAdapterSchema.copy()
+    schema = FormSaveData2ContentAdapterSchema
 
     meta_type = portal_type = 'FormSaveData2ContentAdapter'
     archetype_name = 'Save Data to Content Adapter'
 
     security       = ClassSecurityInfo()
     
+    def createEntry(self):
+        id = self.generateUniqueId()
+        if self.getAvoidSecurityChecks():
+            pt = getToolByName(self, 'portal_types')
+            type_info = pt.getTypeInfo("FormSaveData2ContentEntry")
+            ob = type_info._constructInstance(self, id)
+            return type_info._finishConstruction(ob)
+        else:
+            self.invokeFactory("FormSaveData2ContentEntry", id)
+            return self[id]
+    
     def onSuccess(self, fields, REQUEST=None, loopstop=False):
         """
         magic done here...
         """
-        
-        id = self.invokeFactory("FormSaveData2ContentEntry", self.generateUniqueId())
-        obj = self[id]
+
+        obj = self.createEntry()
         
         for field in self.fgFields():
             name = field.getName()
