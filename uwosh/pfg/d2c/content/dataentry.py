3f2b064d825d8b908fcf55ef97de17a35d2bd38a
saffe <saffe@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-02-27 15:06:54 +0000

    TALES expression did not work when form was submitted anonymously, due to insufficient permissions Fixed.

--- uwosh/pfg/d2c/content/dataentry.py
+++ uwosh/pfg/d2c/content/dataentry.py
@@ -2,6 +2,8 @@
 """
 
 from AccessControl import ClassSecurityInfo
+from AccessControl.SecurityManagement import newSecurityManager, getSecurityManager, setSecurityManager
+
 from Products.ATContentTypes.content.base import registerATCT
 from uwosh.pfg.d2c.config import PROJECTNAME
 from Products.ATContentTypes.content.base import ATCTContent
@@ -26,17 +28,32 @@ class FormSaveData2ContentEntry(ATCTContent):
     archetype_name = 'Save Data to Content Entry'
     
     security       = ClassSecurityInfo()
-    
+
+    security.declareProtected('View', 'Title')    
     def Title(self):
         "generate custom title from the selected form field or the given TALES expression override"
+        
+        # If there's a TALES expression:
+        if self.getParentNode().getRawDynamicTitle():
+        
+            # TALES expr evaluation may require permissions an anonymous user does not have.
+            # So we set up a new security manager and pose as an Owner.
+            old_security_manager = getSecurityManager()
+            user = self.getWrappedOwner()
+            newSecurityManager(None, user)
 
-        # expand title override
-        context = getExprContext(self, self)
-        value = self.getParentNode().getDynamicTitle(expression_context=context)
-        if value:
-           return value
+            # Try evaluating it
+            exprcontext = getExprContext(self,self)
+            try:        
+                value = self.getParentNode().getDynamicTitle(expression_context=exprcontext)
+                setSecurityManager(old_security_manager)
+                return value
+            except Exception, e:
+                # make sure original security manager is reinstated.
+                setSecurityManager(old_security_manager)
+                raise            
 
-        # if not dynamic, then chosen field
+        # Ok. No override, resort to using a selected field
         field = self.getParentNode().getTitleField()
         schema = self.Schema()
         if schema.has_key(field):
