9f31446c60b1eaaab063d3278ca233a9a6eab896
Nathan Van Gheem <vangheem@gmail.com>
2012-12-18 08:54:34 -0600

    bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.3.0b3'
+version = '2.3.0b3dev0'
 
 long_description = (
     read('README.rst') + '\n\n' +
